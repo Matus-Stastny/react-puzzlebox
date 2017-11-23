0639a91c78c4257263832b61c55dc8b48a616fae
Matúš Šťastný <matus.stastny@gmail.com>
2017-11-23 13:28:03 +0100

    Added atoms ProgressBar, Label and EyeIcon and examples

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import EyeIcon from 'components/EyeIcon';
+
+/** Eye icon */
+export default function ExampleOptionalLabel() {
+    return <EyeIcon />;
+}
---
+++ src/docs/examples/Label/ExampleOptionalLabel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'components/Label';
+
+/** Optional label */
+export default function ExampleOptionalLabel() {
+    return <Label label="Example" htmlFor="example" />
+}
