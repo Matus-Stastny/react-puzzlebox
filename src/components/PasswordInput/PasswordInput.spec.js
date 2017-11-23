aacc4c3bdc3b4ae98f381dbbf9583dc514c395bf
Matúš Šťastný <matus.stastny@gmail.com>
2017-11-23 16:01:00 +0100

    Added first snapshot test

--- src/components/PasswordInput/PasswordInput.spec.js
+++ src/components/PasswordInput/PasswordInput.spec.js
@@ -1,12 +1,7 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-
-// Note that this import can be commented out if
-// we declare the jestSetup in package.json because
-// Enzyme functions are globally available for tests.
-import { shallow } from 'enzyme';
-const PasswordInput = require('./PasswordInput');
+import PasswordInput from './PasswordInput';
 
 test('hides password quality by default', () => {
     const tree = renderer.create(<PasswordInput
