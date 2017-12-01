7fd32d6ed1367d334271177d26e39d48700d3629
Matúš Šťastný <matus.stastny@gmail.com>
2017-12-01 09:29:05 +0100

    Clean up, publish preparations

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as EyeIcon } from './EyeIcon/EyeIcon';
+export { default as HelloWorld } from './HelloWorld/HelloWorld';
+export { default as Label } from './Label/Label';
+export { default as PasswordInput } from './PasswordInput/PasswordInput';
+export { default as TextInput } from './TextInput/TextInput';
+export { default as ProgressBar } from './ProgressBar/ProgressBar';
